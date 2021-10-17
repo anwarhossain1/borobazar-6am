6c6ce355cf5fe519eea18c112ffae873f65b3808
anwarhossain1 <irishkhan33@gmail.com>
2021-10-17 14:40:54 +0600

    bestSeller cards added

--- src/components/Cards/BestSeller.jsx
+++ src/components/Cards/BestSeller.jsx
@@ -1,29 +1,41 @@
 import { Button, Card, Col, Row } from "antd";
 import React from "react";
 import "./bestseller.css";
-import freshGreen from "../../assets/images/BestSeller/fresh-green.png";
+import freshGreen from "../../assets/images/BestSeller/i1.png";
+import { dataOfBestSellerProduct } from "./bestSellerData";
 const { Meta } = Card;
 const BestSeller = () => {
+  console.log(dataOfBestSellerProduct);
   return (
     <div className="best__seller">
       <h1>Best seller grocery near you</h1>
       <p>We provide best quality & fresh grocery items near your location</p>
       <div className="cards_row">
         <Row>
-          <Col>
-            <Card
-              className="card"
-              hoverable
-              cover={
-                <img className="card_image" alt="example" src={freshGreen} />
-              }
-            >
-              <button className="cards_button">+</button>
-              <Meta title="$2.64 $2.74" />
-              <p>Fresh Green Leaf Lettuce</p>
-              <p style={{ marginTop: "15px" }}>1 each</p>
-            </Card>
-          </Col>
+          {dataOfBestSellerProduct.map((item) => {
+            return (
+              <Col>
+                <div className="cards_container">
+                  <Card
+                    className="card"
+                    hoverable
+                    cover={
+                      <img
+                        className="card_image"
+                        alt="example"
+                        src={item.image}
+                      />
+                    }
+                  >
+                    <button className="cards_button">+</button>
+                    <Meta title={item.price} />
+                    <p>{item.description}</p>
+                    <p style={{ marginTop: "15px" }}>{item.quuantity}</p>
+                  </Card>
+                </div>
+              </Col>
+            );
+          })}
         </Row>
       </div>
     </div>
